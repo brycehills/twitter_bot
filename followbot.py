d3738549dbd4e6dcf03f9178dbf9b45ba57f75c2
brycehills <hillsbryce@yahoo.com>
2021-01-30 23:14:36 -0800

    updated custom list + other functionalty and sleep
    temp ban from fast following, need to either implement dictionary RNG or stream tweepy api

--- followbot.py
+++ followbot.py
@@ -1,8 +1,15 @@
-import tweepy
+import tweepy,json
 from tweepy import OAuthHandler
 import config as config
 from config import Common as c
-import time, datetime
+import time
+from datetime import datetime, timedelta
+from time import sleep
+
+name = "roman37416126"
+Hours = 5 
+
+qlist = ['Stafford','Rams','Lions','coffee','korea','kpop','mask','wallstreetbets','covid','trump','twitter','github','follow','GME','bitcoin','google','facebook','california','Robinhood','AOC','TRX','Apple','AMC','Tesla','GE','Microsoft','Oracle','Nokia','Amazon','Democrat','Republican','American','Bank','Intel','AMD','Melvin','Uber','a', 'AAA', 'AAAS', 'aardvark', 'Aarhus', 'Aaron', 'ABA', 'Ababa','aback', 'abacus', 'abalone', 'abandon', 'abase', 'abash', 'abate','abbas', 'abbe', 'abbey', 'abbot', 'Abbott', 'abbreviate',"python", "jumble", "easy", "difficult", "answer", "xylophone"]
 
 def main():
 	consumer_key=c.consumer_key
@@ -14,18 +21,39 @@ def main():
 	api = tweepy.API(auth, wait_on_rate_limit=True,
     wait_on_rate_limit_notify=True)
 	return api
+	
+def del_tweets(api, username):
+    page = 1
+    deadend = False
+    tweets = api.user_timeline(username, page = page)
+
+    for i,tweet in enumerate(tweets):
+        if datetime.now()-timedelta(hours=Hours) <= tweet.created_at:
+            print(f'Removed \'{tweet.text}\'')
+            api.destroy_status(tweet.id)
+            sleep(0.5)
+        else:
+            deadend = True
+            return
+    if not deadend:
+        page+=1
+        time.sleep(20)
 		
-def searchNode(api):
-	for tweet in api.search(q="x", count = 1000000000):
-		print(f"{tweet.user.name}:{tweet.text}")
+def searchNode(api,qitem):
+	followcount = 0
+	for tweet in api.search(q = qitem, count = 10):
+		#print(f"{tweet.user.name}:{tweet.text}")
+		time.sleep(5)
 		if not tweet.retweeted:
 			try:
 				tweet.retweet()
+				tweet.favorite()
 			except Exception as e:
 				print("error on retweet\n")
-		if not tweet.user.follow():
+		if followcount < 100:
 			try: 
-				tweet.user.follow()
+				#tweet.user.follow()
+				followcount+=1
 			except Exception as e:
 				print("error on follow\n")
 			
@@ -35,4 +63,6 @@ def printFavorites(api):
 	
 if __name__ == "__main__":
 	api = main()
-	searchNode(api)
+	#del_tweets(api, name)
+	for i in qlist:
+		searchNode(api,i)
